dc8f6aa68632f1ecad0f75b2f579c07d449a926a
Louis <Louis.Medina@flatironschool.com>
2023-01-10 14:44:55 -0500

    complete first deliverable, toggle Dark Mode / Light Mode

--- 02_state_and_events /project_showcase/src/App.js
+++ 02_state_and_events /project_showcase/src/App.js
@@ -1,13 +1,32 @@
+// Components
 import Header from "./components/Header";
 import ProjectForm from "./components/ProjectForm";
 import ProjectList from "./components/ProjectList";
 
+// Data
 import projects from "./projects";
 
+// Hooks
+import { useState } from 'react';
+
 const App = () => {
+
+  // Inverse Data Flow
+
+    // We Have Lifted State (isDarkMode) to App
+    const [isDarkMode, setDarkMode] = useState(true);
+
+    // We have brought over our Helper Function as well 
+    const handleClick = () => {
+      setDarkMode(!isDarkMode);
+    }
+
   return (
-    <div className="App">
-      <Header />
+    <div className={ isDarkMode ? "App" : "App light"}>
+      <Header 
+        isDarkMode={isDarkMode}
+        handleClick={handleClick}
+      />
       <ProjectForm />
       <ProjectList projects={projects} />
     </div>
--- 02_state_and_events /project_showcase/src/components/Header.js
+++ 02_state_and_events /project_showcase/src/components/Header.js
@@ -1,11 +1,23 @@
-const Header = () => {
+const Header = ({ isDarkMode, handleClick }) => {
+  // console.log(useState());
+
+  // Array Destructuring Assigmnment 
+  // const [isDarkMode, setDarkMode] = useState(false);
+  
+  // Declare Our Helper Function
+  // const handleClick = () => {
+  //   setDarkMode(!isDarkMode);
+  // }
+
   return (
     <header>
       <h1>
         <span className="logo">{"//"}</span>
         Project Showcase
       </h1>
-      <button>Dark Mode</button>
+      <button onClick={handleClick}>
+        { isDarkMode ? "Light Mode" : "Dark Mode" }
+      </button>
     </header>
   );
 }
